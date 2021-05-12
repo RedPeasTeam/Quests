7dde49d05325e77b26d892d28125ea943b516da4
Kseniya Blagireva <ksenia062003@gmail.com>
2021-05-13 01:00:24 +0300

    Add new component

--- quest-react-app/src/components/mainPages/questLocation/QuestLocation.js
+++ quest-react-app/src/components/mainPages/questLocation/QuestLocation.js
@@ -1,23 +1,30 @@
-import React, {Component} from "react";
-import { YMaps, Map } from "react-yandex-maps";
+import map from '../../../img/map.jpg';
+import React, {Component} from 'react';
+import * as Api from 'typescript-fetch-api'
 
-const mapData = {
-    center: [53.5439, 27.33 ],
-    zoom: 5,
-  };
-
- 
-  
+const api = new Api.DefaultApi()  
 
 class QuestLocation extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {map: []};
+    this.handleReload()
+ }
+
+ async handleReload() {
+  const response = await api.map({ date: '' });
+  this.setState({ map: response });
+}
     render() {
         return <div className='questLocation'> 
-<div className='title'>Quest location</div>
-  <YMaps>
-    <Map defaultState={mapData} width='900px' height='500px' class='map'>
-    </Map>
-  </YMaps>
-);
+        <div className='map'><img src={map}></img></div>
+{this.state.map.map(
+(element) => <div className='map-item' x={coorX} y ={coorY}>
+<div>{element.name}</div>
+<div>{element.genre}</div>
+</div>
+)}
         </div>
     }
 }
