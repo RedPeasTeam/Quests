c636d92dff8284e77e72f42b84c1f53456d882ed
ddestroys <dasharom0801@gmail.com>
2021-05-15 13:45:46 +0300

    Update Home page

--- quest-react-app/src/components/helpers/questItem/QuestItem.js
+++ quest-react-app/src/components/helpers/questItem/QuestItem.js
@@ -3,14 +3,14 @@ import img from './../../../img/1.jpg';
 
 class QuestItem extends Component {
     render() {
-        const {imgSrc, questName} = this.props;
+        const {imgSrc, questName, difficulty='Сложный', amount='7-10 чел'} = this.props;
         return <div className='quest-item'>
             <img className='quest-img' src={imgSrc} />
             <div className='quest-info'>
                 <div className='quest-name'>{questName}</div>
                 <div className='people'>
-                    <div className='difficulty'>Сложный</div>
-                    <div className='amount'>7-10 чел</div>
+                    <div className='difficulty'>{difficulty}</div>
+                    <div className='amount'>{amount}</div>
                 </div>
             </div>
             <div className='btn'>Бронировать</div>
